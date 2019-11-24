44257f8df48bd4cb31b282914e4ad8ed117d1af2
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-24 22:14:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
-class Dog 
+class Dog
   def name=(dog_name)
-    this_dogs_name=
-dog name 
-  end 
-  def name 
+    this_dogs_name = dog_name
+  end
+ 
+  def name
     this_dogs_name
   end
-end 
+end
 
 lassie = Dog.new 
 lassie.name = "Lassie"
